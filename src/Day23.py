98248190fa00106159711d5f63853bb8c3e8153b
tboulet <tboulet@sidetrade.com>
2021-11-05 18:51:03 +0100

    Day 23 part 2

--- src/Day23.py
+++ src/Day23.py
@@ -1,40 +1,46 @@
-from collections import deque
 from pathlib import Path
 
 
 class CrabCups:
-    def __init__(self, cups):
-        self.cups = deque(cups)
+    def __init__(self, cups, n=None):
+        self.cups = {c: cups[(i+1) % len(cups)] for i, c in enumerate(cups)}
         self.current = cups[0]
+        if n:
+            self.cups.update({i: i+1 for i in range(max(cups) + 1, n)})
+            self.cups[n] = cups[0]
+            self.cups[cups[-1]] = max(cups) + 1
+            self.full_len = n
+        else:
+            self.full_len = len(cups)
 
     def _round(self):
-        self.cups.rotate(-self.cups.index(self.current) - 1)
-        picked_up = [self.cups.popleft() for _ in range(3)]
-        destination = self._find_destination()
-        for i, value in enumerate(picked_up):
-            self.cups.insert(destination + i + 1, value)
-        self.current = self.cups[(self.cups.index(self.current) + 1) % len(self.cups)]
+        b = self.cups[self.current]
+        c = self.cups[b]
+        d = self.cups[c]
+        dest = (self.current - 2) % self.full_len + 1
+        while dest in (b, c, d):
+            dest = (dest - 2) % self.full_len + 1
+        self.cups[self.current] = self.cups[d]
+        self.cups[d] = self.cups[dest]
+        self.cups[dest] = b
+        self.current = self.cups[self.current]
 
-    def ending_labels(self, n, level=1):
+    def run(self, n):
         for i in range(n):
             self._round()
-        self.cups.rotate(-self.cups.index(1))
-        self.cups.popleft()
-        if level == 1:
-            return ''.join(map(str, self.cups))
-        else:
-            return self.cups[0] * self.cups[1]
-
-    def _find_destination(self):
-        label = next((i for i in range(self.current-1, 0, -1) if i in self.cups), max(self.cups))
-        return self.cups.index(label)
+        return self.cups
 
 
 if __name__ == '__main__':
     with open(f'../inputs/{Path(__file__).stem}.txt', 'r') as f:
-        cups = list(map(int, f.read()))
+        cups_list = list(map(int, f.read()))
+    game = CrabCups(list(cups_list)).run(100)
+    res = ''
+    current = game[1]
+    for _ in range(len(game) - 1):
+        res += f'{current}'
+        current = game[current]
+    print(f"The result of first star is {res}.")
 
-    print(f"The result of first star is {CrabCups(list(cups)).ending_labels(100)}.")
-
-    cups.extend(range(max(cups) + 1, 1000001))
-    print(f"The result of second star is {CrabCups(list(cups)).ending_labels(10000000)}.")
+    game = CrabCups(list(cups_list), 1000000).run(10000000)
+    print(f"The result of second star is {game[1] * game[game[1]]}.")
